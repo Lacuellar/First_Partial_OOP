18ecf7a4dccc1e9e6f5a52ef2acfde5c8134453c
Lacuellar <98895078+Lacuellar@users.noreply.github.com>
2022-09-13 09:15:33 -0500

    Create code.py

---
+++ code.py
@@ -0,0 +1,12 @@
+class cliente:
+    def __init__(edad: int, nombre: str) -> None:
+
+
+class bibliotecario:
+    def __init__(nombre: str, edad: int) -> None:
+
+class libro:
+    def __init__(nombre: str) -> None:
+
+class biblioteca:
+    def __init__(nombre_libro) -> None:
